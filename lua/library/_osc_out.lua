a5d0ea6d851abd0a13529c6ca161f7e6e2e3e011
n451 <2020200706@ruc.edu.cn>
2023-12-23 04:28:02 +0800

    osc test 1

--- lua/library/_osc_out.lua
+++ lua/library/_osc_out.lua
@@ -1,9 +1,10 @@
 local tab = require("tabutil")
 
-local losc = require("losc")
-local plugin = require("losc.plugins.udp-socket")
-
-local osc = losc.new({ plugin = plugin:new({ sendAddr = "127.0.0.1", sendPort = 57120 }) })
+local osc = require("osc").new({
+	transport = "udp",
+	sendAddr = "127.0.0.1",
+	sendPort = 57120,
+})
 
 local function generateTypesString(msg)
 	local types = ""
@@ -29,18 +30,17 @@ local osc_out = function(self, x, y)
 	self.name = "osc"
 	self.ports = { { 1, 0, "osc-path" } }
 
-	-- local osc_dest = { "127.0.0.1", 57120 } -- crone
 	local osc_path = {}
 
-	for x = x + 2, 35 do
-		self.ports[#self.ports + 1] = { x - self.x, 0, "osc-path", "input" }
-		if self:glyph_at(x + 1, self.y) == "." then
+	for i = x + 2, 35 do
+		self.ports[#self.ports + 1] = { i - self.x, 0, "osc-path", "input" }
+		if self:glyph_at(i + 1, self.y) == "." then
 			break
 		end
 	end
 
 	for i = 1, #self.ports do
-		l = self:glyph_at(self.x + i, self.y)
+		local l = self:glyph_at(self.x + i, self.y)
 		osc_path[i] = l == "." and "" or l
 	end
 
@@ -48,16 +48,18 @@ local osc_out = function(self, x, y)
 	local values = tab.split(concat_path, ";")
 
 	if #values > 0 then
-		concat_path = values[1]
-		values[1] = nil
-
 		self:spawn(self.ports)
-
+		local msg = {}
+		local param = { "orbit", "sound", "note" }
+		for i, v in pairs(values) do
+			msg[#msg + 1] = param[i]
+			msg[#msg + 1] = tonumber(v) or v
+		end
+		msg["types"] = generateTypesString(msg)
+		msg["address"] = "/dirt/play"
 		if self:neighbor(self.x, self.y, "*") then
-			msg = { ["address"] = "/dirt/play", values }
-			msg["types"] = generateTypesString(msg)
 			local b = osc.new_message(msg)
-			osc.send(b)
+			osc:send(b)
 		end
 	end
 end
